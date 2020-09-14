0c04aa40048b97b35ede0e28c638189a8e5a0891
annie heo <annie.heo@gmail.com>
2020-09-14 16:54:26 -0700

    implement two recursions isEven, sum, factorial

--- src/recursion.js
+++ src/recursion.js
@@ -7,20 +7,48 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+    //if -n return null
+    if (n < 0) {
+        return null;
+    }
+    //if 0 return 1
+    if (n === 0) {
+        return 1; 
+    }
+    //else return factorial of n-1
+    return n * factorial(n - 1)
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    //if empty return 0
+    if (array.length === 0) {
+        return 0;
+    }
+    if (array.length === 1) {
+        return array[0];
+    }
+    return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+    
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+    //
+    var n = Math.abs(n);
+    if (n === 1) {
+        return false;
+    }
+    if (n === 0) {
+        return true; 
+    }
+    return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
